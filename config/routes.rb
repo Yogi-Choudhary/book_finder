b1b50ee4ab17dc7f573736db478ccf9fde00b809
Yogi-Choudhary <yogichoudhary9715@gmail.com>
2021-06-30 20:13:23 +0530

    Add_devise_and_create_user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'books#index'
+  devise_for :users
+
+  root to: 'books#index'
   get '/index' => 'books#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
